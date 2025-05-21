1272f09ad7a00afbaef6b65d30f3c891c56b05e4
Rushi Brahmbhatt <rushibrahmbhatt@Rushis-MacBook-Air.local>
2025-05-21 12:22:23 +0530

    added vercel.json file

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Footer.css'
 import { NavLink } from 'react-router-dom'
+import insta from '../../assets/instagram.png'
+import fb from '../../assets/facebook.png'
 
 
 const Footer = () => {
@@ -15,6 +17,11 @@ const Footer = () => {
       <p><b>Office Address:</b><a href='https://maps.app.goo.gl/myZihcAMpRSowD1dA'>50 Grenfell Street ,Adelaide,5000</a></p>
       <p><b>Contact No:</b><a href='https://wa.me/+610411720025'>+61 0411720025</a></p>
       <p><b>E-mail:</b>info@stepz.com</p>
+      <div className='social'> 
+     
+      <a href='https://www.facebook.com/share/1Ey7NtbwDQ/?mibextid=wwXIfr' >    <img src={fb} alt="" className='fb' /> </a>
+      <a href='https://www.facebook.com/share/1Ey7NtbwDQ/?mibextid=wwXIfr' >    <img src={insta} alt="" /> </a></div>
+
       </div>
        
       <div className='right'> 
@@ -23,7 +30,10 @@ const Footer = () => {
          <NavLink to="/about"><p>About us </p></NavLink>
          <NavLink to="/title"> <p>Services </p></NavLink>
          <NavLink to="/Faq"> <p>FAQ's </p></NavLink>
-        <NavLink  to="/contact"> <p>Contact us </p></NavLink>
+         <NavLink  to="/contact"> <p>Contact us </p></NavLink>
+       
+        
+
         
 
       </div>
